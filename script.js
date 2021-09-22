f3eae59eed9a3f6a5037a531472250a1acb792ac
Makktu <johnmcna@gmail.com>
2021-09-22 19:27:11 +0100

    Progress on display and design

--- script.js
+++ script.js
@@ -4,12 +4,32 @@ function mainLoop() {
     console.log("working");
     displayArea.classList.toggle("expanded");
     displayArea.classList.contains("expanded")
-        ? (displayArea.innerHTML = "Hello World")
-        : (displayArea.innerHTML = "");
+        ? (startButton.textContent = "New 2-Do")
+        : (startButton.textContent = "Get Started");
 }
 
 const startButton = document.getElementById("start-btn");
 
-startButton.addEventListener("click", mainLoop);
+startButton.addEventListener("click", () => {
+    if (!displayArea.classList.contains("expanded")) {
+        mainLoop();
+    } else {
+        let new2do = "Hello Again World";
+        toDoList.push(new2do);
+        let display = "";
+        toDoList.forEach((task) => {
+            display += task + "<br>";
+        });
+        mainDisplay.innerHTML = display;
+    }
+});
 
 const displayArea = document.querySelector(".display-area");
+
+const mainDisplay = document.querySelector(".main-display");
+
+let toDoList = [];
+
+let deletedToDoList = [];
+
+let completedToDoList = [];
