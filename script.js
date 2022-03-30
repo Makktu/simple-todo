db29dbb834d14370c9d9c036b01282e1876367df
Makktu <johnmcna@gmail.com>
2022-03-30 18:29:30 +0100

    completely changed

--- script.js
+++ script.js
@@ -1,35 +1,70 @@
 "use strict";
 
-function mainLoop() {
-    console.log("working");
-    displayArea.classList.toggle("expanded");
-    displayArea.classList.contains("expanded")
-        ? (startButton.textContent = "New 2-Do")
-        : (startButton.textContent = "Get Started");
-}
+const text = document.getElementById("text");
+const addTaskButton = document.getElementById("add-task-btn");
+const saveTaskButton = document.getElementById("save-todo-btn");
+const listBox = document.getElementById("listBox");
+const saveInd = document.getElementById("saveIndex");
 
-const startButton = document.getElementById("start-btn");
+let todoArray = [];
 
-startButton.addEventListener("click", () => {
-    if (!displayArea.classList.contains("expanded")) {
-        mainLoop();
+addTaskButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let todo = localStorage.getItem("todo");
+    if (todo === null) {
+        todoArray = [];
     } else {
-        let new2do = "Hello Again World";
-        toDoList.push(new2do);
-        let display = "";
-        toDoList.forEach((task) => {
-            display += task + "<br>";
-        });
-        mainDisplay.innerHTML = display;
+        todoArray = JSON.parse(todo);
     }
+    todoArray.push(text.value);
+    text.value = "";
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
 });
 
-const displayArea = document.querySelector(".display-area");
+function displayTodo() {
+    let todo = localStorage.getItem("todo");
+    if (todo === null) {
+        todoArray = [];
+    } else {
+        todoArray = JSON.parse(todo);
+    }
+    let htmlCode = "";
+    todoArray.forEach((list, ind) => {
+        htmlCode += `<div class='flex mb-4 items-center'>
+    <p class='w-full text-grey-darkest'>${list}</p>
+    <button onclick='edit(${ind})' class='flex-no-shrink p-2 ml-4 mr-2 border-2 rounded text-white text-grey bg-green-600'>Edit</button>
+    <button onclick='deleteTodo(${ind})' class='flex-no-shrink p-2 ml-2 border-2 rounded text-white bg-red-500'>Delete</button>
+   </div>`;
+    });
+    listBox.innerHTML = htmlCode;
+}
 
-const mainDisplay = document.querySelector(".main-display");
+function deleteTodo(ind) {
+    let todo = localStorage.getItem("todo");
+    todoArray = JSON.parse(todo);
+    todoArray.splice(ind, 1);
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+}
 
-let toDoList = [];
+function edit(ind) {
+    saveInd.value = ind;
+    let todo = localStorage.getItem("todo");
+    todoArray = JSON.parse(todo);
+    text.value = todoArray[ind];
+    addTaskButton.style.display = "none";
+    saveTaskButton.style.display = "block";
+}
 
-let deletedToDoList = [];
-
-let completedToDoList = [];
+saveTaskButton.addEventListener("click", () => {
+    let todo = localStorage.getItem("todo");
+    todoArray = JSON.parse(todo);
+    let id = saveInd.value;
+    todoArray[id] = text.value;
+    addTaskButton.style.display = "block";
+    saveTaskButton.style.display = "none";
+    text.value = "";
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+});
