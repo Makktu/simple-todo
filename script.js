cacd6d9f4285175b6a9aea21509a90e557a3d147
Makktu <johnmcna@gmail.com>
2022-03-30 18:44:48 +0100

    colour scheme changed

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function displayTodo() {
     let htmlCode = "";
     todoArray.forEach((list, ind) => {
         htmlCode += `<div class='flex mb-4 items-center'>
-    <p class='w-full text-grey-darkest'>${list}</p>
+    <p class='w-full text-white'>${list}</p>
     <button onclick='edit(${ind})' class='flex-no-shrink p-2 ml-4 mr-2 border-2 rounded text-white text-grey bg-green-600'>Edit</button>
     <button onclick='deleteTodo(${ind})' class='flex-no-shrink p-2 ml-2 border-2 rounded text-white bg-red-500'>Delete</button>
    </div>`;
